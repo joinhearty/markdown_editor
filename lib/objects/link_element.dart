032c2092f6babcb8acf61848b2bcab36b5436f19
Morgan <mrgnhnt96@gmail.com>
2023-10-03 17:34:08 -0500

    refactor: expose pattern of link element

--- lib/objects/link_element.dart
+++ lib/objects/link_element.dart
@@ -3,11 +3,11 @@ import 'package:flutter_app/objects/html_element.dart';
 class LinkElement extends HtmlElement {
   const LinkElement();
 
+  static RegExp pattern =
+      RegExp(r'\[(.+?)\]\((.+?)\)', multiLine: true, dotAll: true);
+
   @override
   String replace(String input) {
-    final pattern =
-        RegExp(r'\[(.+?)\]\((.+?)\)', multiLine: true, dotAll: true);
-
     return input.replaceAllMapped(pattern, (match) {
       final text = match.group(1);
       final link = match.group(2);
