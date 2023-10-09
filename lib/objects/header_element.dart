4a5b7ddaf3ccb840bb328b54675bf62860a2e413
Morgan <mrgnhnt96@gmail.com>
2023-10-09 12:21:39 -0500

    fix: allow max 6, remove starting and ending symbols from pattern

--- lib/objects/header_element.dart
+++ lib/objects/header_element.dart
@@ -8,7 +8,7 @@ class HeaderElement extends HtmlElement {
     final pattern = RegExp(r'^(#+) (.+)$', multiLine: true);
 
     return input.replaceAllMapped(pattern, (match) {
-      final headerLevel = match.group(1)!.length;
+      final headerLevel = match.group(1)!.length.clamp(0, 6);
       final text = match.group(2);
 
       return '<h$headerLevel>$text</h$headerLevel>';
@@ -17,7 +17,7 @@ class HeaderElement extends HtmlElement {
 
   @override
   String toMarkdown(String input) {
-    final pattern = RegExp(r'^<h([1-6])>(.+)</h[1-6]>$', multiLine: true);
+    final pattern = RegExp(r'<h([1-6])>(.+)</h[1-6]>', multiLine: true);
 
     return input.replaceAllMapped(pattern, (match) {
       final headerLevel = match.group(1);
